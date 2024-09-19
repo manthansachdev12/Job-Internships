9e78f5ef94675f8055a65ae34c9e47b9f4cff922
manthansachdev12 <156173895+manthansachdev12@users.noreply.github.com>
2024-09-20 01:20:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function exploreJobs() {
+    alert("Redirecting to the job opportunities page...");
+    window.location.href = "job.html"; // You can link to the actual jobs page.
+}
+
+function viewInternships() {
+    alert("Redirecting to the internships page...");
+    window.location.href = "internships.html"; // You can link to the actual internships page.
+}
+
+function findMentor() {
+    alert("Redirecting to the mentorship programs page...");
+    window.location.href = "mentorship.html"; // You can link to the actual mentorship page.
+}
